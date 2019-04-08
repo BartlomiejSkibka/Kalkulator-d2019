557ea5aaee25f1afcb6fe7e4058a22b4da753285
Bartłomiej Skibka <46480@student.ue.poznan.pl>
2019-04-08 17:50:53 +0200

    nowa funkcja dodawania gotowa

--- kalkulato.py
+++ kalkulato.py
@@ -1,6 +1,10 @@
 print('To jest prosty kalkulator')
 def dodaj(a,b):
-	return
+	return a+basestring
 	
+x = int(input())
+y = int(input())
+
+print(dodaj(x, y))	
 print('Koniec Programu')
-print('papa')
+print('Papaj')
